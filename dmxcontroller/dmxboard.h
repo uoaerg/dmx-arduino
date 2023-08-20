b505c3e02bed2a5c1b0bce88e484d055dabd4a92
Gorry Fairhurst <gorry@erg.abdn.ac.uk>
2023-08-20 09:55:48 +0100

    Update dmxboard.h to include title as comment

--- dmxcontroller/dmxboard.h
+++ dmxcontroller/dmxboard.h
@@ -1,25 +1,26 @@
+/* DMXBoard */
 #ifndef DMXBOARD_H
-#define DMXBOARD_H
+  #define DMXBOARD_H
 
-#define REV_II 2
-#define REV_IIIA 3
+  #define REV_II 2
+  #define REV_IIIA 3
 
-/* #define REV	REV_II */
-#define REV REV_IIIA
+  /* #define REV	REV_II */
+  #define REV REV_IIIA
 
-// #pragma message "Board and software revision defined in dmxboard.h."
-#ifndef REV
-// #pragma message "Please set a board revision in dmxboard.h"
-#endif
+  // #pragma message "Board and software revision defined in dmxboard.h."
+  #ifndef REV
+    // #pragma message "Please set a board revision in dmxboard.h"
+  #endif
 
-#define GIT "Release 1.0 (08-23 GF)"	/* please set manually before flashing */
+  #define GIT "Release 1.0 (08-23 GF)"	/* please set manually before flashing */
 
-#if REV == REV_II
-#define DMXBOARDREV "dmxboard rev II"
-#endif
+  #if REV == REV_II
+    #define DMXBOARDREV "dmxboard rev II"
+  #endif
 
-#if REV == REV_IIIA
-#define DMXBOARDREV "dmxboard rev IIIA"
-#endif
+  #if REV == REV_IIIA
+    #define DMXBOARDREV "dmxboard rev IIIA"
+  #endif
 
 #endif //  DMXBOARD_H
